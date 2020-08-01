30cd510f287c2f3ff4a7c8ecd32d37b2e90594a0
SR <sarath.raveendran@litmus7@.com>
2020-08-01 22:21:37 +0530

    Adding missing files

---
+++ DiffableDataSource2/Diffable/BooksDiffableDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  BooksDiffableDataSource.swift
+//  DiffableDataSource2
+//
+//  Created by SR on 8/1/20.
+//  Copyright © 2020 SR. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/*
+ * Out of the box, the diffable data source is pretty plain.
+ * But since it conforms to UITableViewDataSource it can do anything you might be already doing in your current UITableViewDataSource implementation.
+ * All you need to do is define your own UITableViewDiffableDataSource and override the methods of features you want to implement
+ */
+
+
+
+
+class BooksDiffableDataSource: UICollectionViewDiffableDataSource<Section, TextBook> {
+ 
+   
+}
